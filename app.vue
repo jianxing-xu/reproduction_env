08a2306ba68ad88c069d01d728b4c40b00bb8861
Jianxing <jinaxingxu@gmail.com>
2025-01-02 18:13:22 +0800

    init

---
+++ app.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+const config = useRuntimeConfig();
+
+console.log(config.public.apiBase)
+</script>
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
